e47f9a9619d14c947990ca0e85be19747909a45b
Ariel Tapia <arieltapia16@gmail.com>
2017-02-14 21:15:45 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import Header from './components/header/header';
+
+class App extends Component {
+  render () {
+    return (
+      <Header />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import LogoImg from './img/elpapa.png';
+
+export default class Header extends React.Component {
+  render () {
+    return (
+      <Navbar >
+        <div className='Logo'><img src={LogoImg} width='100px' /> <div className='title'>EL PAPA FOREVER</div></div>
+      </Navbar>
+    );
+  }
+}
